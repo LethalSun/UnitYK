a97489531a9a2940afce2a3630a126b9426c383e
DESKTOP-3VOQMPI\LongShore <massivedynamicplay@gmail.com>
2017-07-14 11:07:29 +0900

    보드게임 배틀쉽 카메가 움직이는 부분에서 막힘  커서 가두기를 할수 있을까? 아니면 진짜 커서는 숨기고 게임 내에 새로운 커서를 만들어서 그냥 마우스 움직임을 입력 받아서 움직여야 할까? 그러면 그 오브젝트의 위치는 어떻게 잡아야 하지?그리고 뷰 프러스텀 바깥으로 나가지 못하게 하려면 충돌을 이용해야 할까? 일단 스크린, 월드, 뷰 포트 의 좌표 변환 함수가 있다는것을 찾음

---
+++ k/BS1/Assets/Scripts/HandleCursor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleCursor : MonoBehaviour {
+
+    public Texture2D BasicCursor;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+	// Use this for initialization
+	void Start () {
+        //Screen.LockCursor
+        // Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = true;
+        //Cursor.lockState = CursorLockMode.Confined;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = true;
+        Cursor.SetCursor(BasicCursor, hotSpot, cursorMode);
+        //Cursor.lockState = CursorLockMode.Confined;
+    }
+}
