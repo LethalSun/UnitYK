8354727b8c7209c1e0f2047856c616c12b450bc4
DESKTOP-3VOQMPI\LongShore <massivedynamicplay@gmail.com>
2017-08-18 14:02:19 +0900

    no message

--- k/BS1/Assets/Scripts/DeployShip.cs
+++ k/BS1/Assets/Scripts/DeployShip.cs
@@ -5,31 +5,18 @@ using UnityEngine;
 public class DeployShip : MonoBehaviour
 {
 
-    public GameObject ship1;
-    public GameObject ship2;
-    public GameObject ship3;
-    public GameObject ship4;
-    public GameObject ship5;
-
-
     private GameObject selectedShip = null;
     public int shipNum = 0;
     public int shipNum2 = 0;
     private GameObject target;
     private GameObject targetBefore;
     public GameObject initTile;
-    private GameObject[] ships;
     private int rotaion = 1;
-    
+
+    public GameObject harbor;
     private void Start()
     {
         targetBefore = initTile;
-        ships = new GameObject[5];
-        ships[0] = ship1;
-        ships[1] = ship2;
-        ships[2] = ship3;
-        ships[3] = ship4;
-        ships[4] = ship5;
     }
 
     GameObject GetCursorObject()
@@ -46,7 +33,6 @@ public class DeployShip : MonoBehaviour
             target = hit.collider.gameObject;
             if (target.layer == 31)
             {
-         //       Debug.Log("Hit");
                 targetBefore = target;
                 return target;
             }
@@ -56,37 +42,35 @@ public class DeployShip : MonoBehaviour
         return target;
     }
 
-    void Checkselectedship()
+    GameObject ClickedShip()
     {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+            GameObject target = null;
+            int rayMask = 1 << 29;
 
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            MakeShip(1);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            MakeShip(2);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            MakeShip(3);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            MakeShip(4);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha5))
-        {
-            MakeShip(5);
-        }
-        else if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            shipNum = 0;
-            if (selectedShip != null)
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, rayMask) == true)
             {
-                Destroy(selectedShip);
+
+                target = hit.collider.gameObject;
+                if (target.layer == 29)
+                {
+                    targetBefore = target;
+                    return target;
+                }
+                target = null;
             }
+
+            return target;
         }
+        else
+        {
+            return null;
+        }
+        
+       
     }
 
     public void SelectShip1()
@@ -97,7 +81,6 @@ public class DeployShip : MonoBehaviour
 
     public void SelectShip2()
     {
-        Debug.Log("shipnum =" + shipNum2);
         shipNum2 = 2;
         MakeShip(shipNum2);
     }
@@ -125,6 +108,8 @@ public class DeployShip : MonoBehaviour
 
         if (selectedShip == null)
         {
+            Debug.Log("ship null");
+            selectedShip = ClickedShip();
             return;
         }
 
@@ -146,6 +131,7 @@ public class DeployShip : MonoBehaviour
             if (target != null)
             {
                 target.GetComponent<ShipManager>().shipOnTheTile = selectedShip;
+                selectedShip.GetComponent<ShipProperties>().isDeployed = true;
                 selectedShip = null;
                 shipNum = 0;
             }
@@ -154,23 +140,16 @@ public class DeployShip : MonoBehaviour
 
     void MakeShip(int shipN)
     {
+        var ships = harbor.GetComponent<MakeShip>().ShipDock;
 
-
-        if (shipNum == shipN)
+        foreach(var elem in ships)
         {
-            return;
-        }
-        else
-        {
-
-            shipNum = shipN;
-            if(selectedShip !=null)
+            var property = elem.GetComponent<ShipProperties>();
+            if(property.shipId == shipN && property.isDeployed == false)
             {
-                Destroy(selectedShip);
+                selectedShip = elem;
+                break;
             }
-            selectedShip = Instantiate(ships[shipNum - 1]) as GameObject;
-            //TODO:
-            //selectedShip.
         }
     }
 
