53a96b5850aae9ac8111139ef40bae0a813dfda9
DESKTOP-3VOQMPI\LongShore <massivedynamicplay@gmail.com>
2017-09-23 22:13:56 +0900

    배열 수정

--- k/BS1/Assets/Scripts/Tile/DeployTile.cs
+++ k/BS1/Assets/Scripts/Tile/DeployTile.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class DeployTile : MonoBehaviour {
 
     //TODO:각각의 2차원배열을 1차원 배열로 수정
-    public GameObject[,] tiles;
+    public GameObject[] tiles;
     public GameObject tile;
-    public GameObject[,] enemyTiles;
+    public GameObject[] enemyTiles;
     public GameObject enemyTile;
     public float startTileX = -60.0f;
     public float startTileZ = -15.0f;
@@ -16,14 +16,14 @@ public class DeployTile : MonoBehaviour {
     public float tileOffsetX = 10.0f;
     public float tileOffsetZ = -10.0f;
     public int tileNumX = 13;
-    public int tileNumZ = 18;
+    public int tileNumZ = 8;
 
 
     // Use this for initialization
     void Start () {
 
-        tiles = new GameObject[tileNumX, tileNumZ];
-        enemyTiles = new GameObject[tileNumX, tileNumZ];
+        tiles = new GameObject[tileNumX * tileNumZ];
+        enemyTiles = new GameObject[tileNumX * tileNumZ];
         float posX = startTileX;
         float posZ = startTileZ;
 
@@ -34,25 +34,26 @@ public class DeployTile : MonoBehaviour {
         {
             for(int z = 0;z< tileNumZ; ++z )
             {
-                tiles[x, z] = Instantiate(tile) as GameObject;
-                tiles[x, z].transform.position = new Vector3(posX, 10.0f, posZ);
-                tiles[x, z].GetComponent<TileInfo>().rowNumber = x;
-                tiles[x, z].GetComponent<TileInfo>().columnNumber = z;
+                
+                tiles[tileNumZ * x + z] = Instantiate(tile) as GameObject;
+                tiles[tileNumZ * x + z].transform.position = new Vector3(posX, 10.0f, posZ);
+                tiles[tileNumZ * x + z].GetComponent<TileInfo>().rowNumber = x;
+                tiles[tileNumZ * x + z].GetComponent<TileInfo>().columnNumber = z;
                 posZ += tileOffsetZ;
 
-                enemyTiles[x,z] = Instantiate(enemyTile) as GameObject;
-                enemyTiles[x, z].transform.position = new Vector3(posEx, 10.0f, posEz);
-                enemyTiles[x, z].GetComponent<TileInfo>().rowNumber = x;
-                enemyTiles[x, z].GetComponent<TileInfo>().columnNumber = z;
+                enemyTiles[tileNumZ * x + z] = Instantiate(enemyTile) as GameObject;
+                enemyTiles[tileNumZ * x + z].transform.position = new Vector3(posEx, 10.0f, posEz);
+                enemyTiles[tileNumZ * x + z].GetComponent<TileInfo>().rowNumber = x;
+                enemyTiles[tileNumZ * x + z].GetComponent<TileInfo>().columnNumber = z;
 
                 //TODO: 테스트 코드 나중에 값을 받아와서 채워넣어야 함.
                 if(Random.Range(0,2) == 0)
                 {
-                    enemyTiles[x, z].GetComponent<EnemyTileProperty>().isThereEnemyShip = false;
+                    enemyTiles[tileNumZ * x + z].GetComponent<EnemyTileProperty>().isThereEnemyShip = false;
                 }
                 else
                 {
-                    enemyTiles[x, z].GetComponent<EnemyTileProperty>().isThereEnemyShip = true;
+                    enemyTiles[tileNumZ * x + z].GetComponent<EnemyTileProperty>().isThereEnemyShip = true;
                 }
 
                 posEz += tileOffsetZ;
